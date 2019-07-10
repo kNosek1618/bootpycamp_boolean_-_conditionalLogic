564b8406a2901286ba67736a01c4d81d0a62f7a7
root <kNosek1618>
2019-07-10 18:14:24 +0000

    upgrade code

--- EntryAge.py
+++ EntryAge.py
@@ -1,12 +1,16 @@
 
 age = input("How old are you:")
 
-if age != '':
-    if int(age) >= 18 and int(age) < 21:
-        print("You can enter, but need a wristband!")
-    elif int(age) >= 21:
-        print("You are good to enter and can drink")
+if age == type(str):
+    if age:
+        age = int(age)
+        if age >= 18 and age < 21:
+            print("You can enter, but need a wristband!")
+        elif age >= 21:
+            print("You are good to enter and can drink")
+        else:
+            print("You can't come in, little one! :(")
     else:
-        print("You can't come in, little one! :(")
+        print("please, enter the age.")
 else:
-    print("please, enter the age.")
+    print("Please, enter the number.")
