4c5b0702763a617821b5c6bc3c1949ed52366ad1
root <kNosek1618>
2019-07-10 18:04:36 +0000

    new 'AgeEntry' app

--- IsVs'=='.py
+++ IsVs'=='.py
@@ -9,16 +9,25 @@ print(a is 1)
 if a is 1:
     print("Example when 'is' operator is equal '==' operator\n")
 
-
+#example when 'is' operator it's different
 x = [1, 2, 3]
 y = [1, 2, 3]
+z = y
 
 if x == y:
     print("x list is equal y list but,")
 else:
-    print("x list isn't equal y list but,")
+    print("x list isn't equal y list and,")
 
+#x and y variables is different because the list is object
+#the objects is not be equal
 if x is y:
     print("x list is y list.")
 else:
     print("x list isn't y list.")
+
+#'is' is only truthy if the variables reference the same item in memory.
+if y is z:
+    print("y is z because this is a copy of the list.")
+
+
