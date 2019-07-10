d411a125b62ab928fae94e755bdca81c2828e332
root <kNosek1618>
2019-07-10 14:42:28 +0000

    'and', 'or', 'not' operators

---
+++ LogicalOperators.py
@@ -0,0 +1,16 @@
+
+#LOGICAL OPERATORS
+
+x = 5
+
+#examples of using operators 'and' 'or' 'not'
+if x>2 and x<6:
+    print("'and' operator is working!\n")
+
+
+if x>2 or x>6:
+    print("'or' operator is working!\n")
+
+
+if not x>6:
+    print("denial of False gives the True.")
